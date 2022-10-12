8c144c4070ab9336c25317fd74fba6d968b7c748
Asadullah246 <asadm2258@gmail.com>
2022-10-12 08:49:21 +0600

    footer solved

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -21,11 +21,11 @@ const Footer = () => {
             </div>
             {Divider}
          
-            <MDBFooter bgColor='' className='text-center text-lg-start text-white'>
-                <section className=''>
-                    <MDBContainer className='text-center text-md-start mt-5 footerContainer'>
-                        <MDBRow className='mt-3'>
-                            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+            <div className='text-start text-white'>
+                <section className='footerDiv'>
+                    <div className='container-fluid mt-5  footerContainer'>
+                        <div className='row row-cols-2 row-cols-md-2 row-cols-lg-4 mt-3'>
+                            <div className='col mx-auto mb-4'>
                                 <h2 className='text-uppercase fw-bold mb-4'>
                                     
                                     LOGO
@@ -36,16 +36,16 @@ const Footer = () => {
                                 <p>
                                     +880 1784453204
                                 </p>
-                                <div style={{display:"flex ", alignItems:"center", width:"fit-content"}} className="socialIcons  text-md-start">
+                                <div style={{display:"flex ", alignItems:"left", width:"fit-content"}} className="socialIcons  text-start">
                                     <p style={{fontSize:"1.5em"}}><RiFacebookCircleLine/></p>
                                     <p><GrTwitter/></p>
                                     <p><AiOutlineInstagram/></p>
                                     <p><ImLinkedin/></p>
                                 </div>
-                            </MDBCol>
+                            </div>
 
-                            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-                                <h6 className='text-uppercase fw-bold mb-4'>Quick Links</h6>
+                            <div className='col mx-auto mb-4'>
+                                <h6 className='text-uppercase fw-bold mb-4 mt-2 mt-lg-0'>Quick Links</h6>
                                 <p>
                                     <a href='#!' className='text-reset'>
                                         Products
@@ -62,9 +62,11 @@ const Footer = () => {
                                     </a>
                                 </p>
                                
-                            </MDBCol>
+                            </div>
 
-                            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+                            <div
+                            //  md="3" lg="2" xl="2"
+                              className='col mx-auto mb-4'>
                                 <h6 className='text-uppercase fw-bold mb-4'>Legal</h6>
                                 <p>
                                     <a href='#!' className='text-reset'>
@@ -77,27 +79,27 @@ const Footer = () => {
                                     </a>
                                 </p>
                                
-                            </MDBCol>
+                            </div>
 
-                            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+                            <div className='col mx-auto mb-md-0 mb-4'>
                                 <h6 className='text-uppercase fw-bold mb-4'>Supported payment systems</h6>
-                                <div className='d-flex justify-content-start align-items-center gap-3'>
+                                <div className='d-flex justify-content-start align-items-center flex-wrap gap-2 gap-md-3'>
                                     <img src="images/footer/Vector (3).png" alt="" />
                                     <p className='mb-0'>VISA</p>
                                     <p className='mb-0'>stripe</p>
                                     <img src="images/footer/Frame 38183.png" alt="" />
                                 </div>
                                
-                            </MDBCol>
-                        </MDBRow>
-                    </MDBContainer>
+                            </div>
+                        </div>
+                    </div>
                 </section>
                 {Divider}
 
                 <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
                    Copyright ©2022 Mystique vedic miracles. All Rights Reserved.   
                 </div>
-            </MDBFooter>
+            </div>
         </div>
     );
 };
